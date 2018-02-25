cd3b1043034f8e3a91981114264024018a75784d
Atlantisit <you@example.com>
2018-02-25 23:26:06 +0300

    main_text

--- crossvszero.py
+++ crossvszero.py
@@ -1,13 +1,42 @@
-# Function list:
-# instruct() - instruction for game;
-# ask_yes_no(question) - accept text, return 'y' or 'n';
-# ask_number(question, low, higt()) - accept text, return an integer in the range low-hight;
-# chip() - Choose who play first. Return tipes of chips (X or 0);
-# new_board() - create new game board, return the board;
-# available_moves() -  create list legal moves. Accepts the board, return list of available moves;
-# winner(board) - print winner in the game. Accepts the board, return type of winner chips (X or O), or a draw.
-# human_move(board, human) - human player's move. Accepts the board, type of chips player's, return human player's move;
-# computer_move(board, computer, human) - Accepts the board, type of chips player's, return computer's move;
-# next_turn(turn) - Accepts tupes of chips, return tupes of chips;
-# congrat_winner(the_winner, computer, human) - Congrat the winner, or ascertains a draw. Accepts tupes of chips.
-
+import random
+#Global consts
+X = 'X'
+O = 'O'
+EMPTY = " "
+TIE = "A draw"
+NUM_SQUARES = 9
+#Part with functions
+def instruct():
+    print(
+        """
+        Wellcome to the game Tic-Tac-Toe!
+        Look at this board:\n
+        0 | 1 | 2
+        3 | 4 | 5
+        6 | 7 | 8
+        \nEnter a number from 0 to 8 to make a move. Good luck!
+        """
+    )
+def ask_yes_no(question):
+    response = None
+    while response not in ("y", "n"):
+        response = input(question),lower()
+    return response
+def ask_number(question, low, high):
+    response = None
+    while respornse not in range (low, high):
+        response = int(input(question))
+        return response
+def chip():
+    number = random.choice((X,O))
+    if number == X:
+        print("Your move will be first!")
+        human = X
+    else:
+        print("I will be first!")
+        computer = X
+def new_board():
+    board = []
+    for square in range(NUM_SQUARES):
+        board.append(EMPTY)
+    return board
