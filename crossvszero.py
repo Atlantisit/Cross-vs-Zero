2b925ff197fc618e4242cbf237d803b8a5b1e4f8
Atlantisit <you@example.com>
2018-03-05 19:58:25 +0300

    main_text

--- crossvszero.py
+++ crossvszero.py
@@ -40,3 +40,40 @@ def new_board():
     for square in range(NUM_SQUARES):
         board.append(EMPTY)
     return board
+def display_board(board):
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "-------------")
+    print("\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "-------------")
+    print("\t", board[6], "|", board[7], "|", board[8], "/n")
+def available_moves(board):
+    moves = []
+    for square in range(NUM_SQUARES):
+        if board[square] == EMPTY:
+            moves.append(square)
+    return moves
+def winner(board):
+    WAYS_TO_WIN = ((0, 1, 2),
+                    (3, 4, 5),
+                    (6, 7, 8),
+                    (0, 3, 6),
+                    (1, 4, 7),
+                    (2, 5, 8),
+                    (0, 4, 8),
+                    (2, 4, 6))
+    for row in WAYS_TO_WIN:
+        if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
+            winner = board[row[0]]
+            return winner
+        if EMPTY not in board:
+            return TIE
+    return None 
+def human_move(board, human):
+    legal = display_board(board)
+    move = None
+    while move not in legal:
+        move = ask_number("Your move. Choose one of the field 1-8: ", 0, NUM_SQUARES)
+        if move not in legal:
+            print("\nThis field is busy. Choose other field.")
+        print ("Ok")
+        return move
